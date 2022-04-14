15984f1047a7050034d8aa950a58a488189ad2e3
jacob nordan <jacob.nordan113322@gmail.com>
2022-04-14 13:22:58 -0700

    add a switch case for each operator that could be used, and throwing an error if the operator isnt a correct given operator

--- main.cpp
+++ main.cpp
@@ -15,4 +15,28 @@ int main() {
 
   cout << "Enter your second operands: ";
   cin >> num2;
+  
+  switch(op) {
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+
+    default:
+      // If the operator is other than +, -, * or /, error message is shown
+      cout << "Error! operator is not correct";
+      break;
+  }
+  return 0;
 }
